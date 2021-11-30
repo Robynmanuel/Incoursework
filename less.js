09881148c3f476024423502aaf99113da30891b8
Robynmanuel <55141470+Robynmanuel@users.noreply.github.com>
2021-11-30 20:15:32 +0100

    Eighth commit

--- less.js
+++ less.js
@@ -128,3 +128,12 @@ function srt() {
       x.style.display = "block";
     }
   }
+
+  
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
